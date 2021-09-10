b19125a478961164369fcfd29521b0f822e083d3
kitutune <79749395+kitutune@users.noreply.github.com>
2021-09-10 21:21:16 +0900

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all', // 末尾のカンマあり
+  tabWidth: 2, // tab の長さは半角スペース 2 つ
+  semi: true, // セミコロンあり
+  singleQuote: true, // シングルクォーテーションに統一
+  jsxSingleQuote: true, //jsx もシングルクォーテーションに統一
+  printWidth: 100, // １ 行の最大文字数 100
+  // parser: 'typescript',
+};
