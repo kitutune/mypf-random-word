3052f10af2e386263b30ab41bb6fcf30cbabe625
kitutune <p.kitutune@gmail.com>
2021-09-12 02:08:25 +0900

    ✨ BurgerIconcomponent作成

---
+++ src/components/BurgerMenu/BurgerIcon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const BurgerIcon = () => {
+  return (
+    <svg className='h-12 w-12' viewBox='0 0 24 24' fill='none'>
+      <path
+        d='M4 6H20M4 12H20M4 18H20'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      ></path>
+    </svg>
+  );
+};
