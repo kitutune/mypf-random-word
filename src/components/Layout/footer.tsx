50e774346dd06dfaa7bd628b41887887df98321e
kitutune <p.kitutune@gmail.com>
2021-09-11 17:42:41 +0900

    🎨headerとfooterのコードをLayoutの中に移動

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
-
 export const Footer: React.FC = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <a
+        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Powered by{' '}
+        <span>
+          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+        </span>
+      </a>
+    </footer>
+  );
 };
