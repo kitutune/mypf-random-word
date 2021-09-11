fa984d19bd623c97c7bda3f653e99febf1a8aef9
kitutune <p.kitutune@gmail.com>
2021-09-11 17:43:42 +0900

    🎨supabaseのURLとKEYの引き方を変更

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -1,12 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-// 環境変数のロード
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-// 環境変数が設定されていなかったらエラーを投げる
-if (!SUPABASE_URL) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
-}
-if (!SUPABASE_KEY) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY');
-}
-export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
