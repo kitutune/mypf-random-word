2be477b89404a67852e73ce99b8d2b011c2032a3
kitutune <p.kitutune@gmail.com>
2021-09-12 02:11:55 +0900

    🎨HouseIconとBurgerMenuにコードを移動

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
+import { BurgerMenu } from 'components/BurgerMenu';
+import { HouseIcon } from 'components/BurgerMenu/HouseIcon';
 export const Header: React.FC = () => {
   return (
     <>
@@ -14,34 +16,10 @@ export const Header: React.FC = () => {
             <div className='md:flex justify-between items-center'>
               <div className='flex justify-between items-center'>
                 <a href='#' className='text-white'>
-                  <svg
-                    className='w-8 mr-2 fill-current'
-                    xmlns='http://www.w3.org/2000/svg'
-                    data-name='Capa 1'
-                    viewBox='0 0 16.16 12.57'
-                  >
-                    <defs></defs>
-                    <path d='M14.02 4.77v7.8H9.33V8.8h-2.5v3.77H2.14v-7.8h11.88z'></path>
-                    <path d='M16.16 5.82H0L8.08 0l8.08 5.82z'></path>
-                  </svg>
+                  <HouseIcon />
                 </a>
                 <div className='md:hidden'>
-                  <button className='text-white focus:outline-none'>
-                    <svg
-                      className='h-12 w-12'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M4 6H20M4 12H20M4 18H20'
-                        stroke='currentColor'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      ></path>
-                    </svg>
-                  </button>
+                  <BurgerMenu />
                 </div>
               </div>
               <div className='hidden md:flex items-center'>
