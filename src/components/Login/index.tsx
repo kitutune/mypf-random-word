d8fe8fcf6c6745c83180f0c83ae66770edca6f08
kitutune <p.kitutune@gmail.com>
2021-09-11 18:09:45 +0900

    ♻️ nullの処理を追記

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 import { supabase } from 'libs/supabase';
 
 export const Login = () => {
-  const [session, setSession] = useState();
+  const [session, setSession] = useState<object | null>();
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
       setSession(session);
     });
-
+    if (authListener === null) {
+      return;
+    }
     return () => {
       authListener.unsubscribe();
     };
