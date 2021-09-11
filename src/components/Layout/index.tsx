88f0419011bd6c1a6892568a299a8faaeda08b22
kitutune <p.kitutune@gmail.com>
2021-09-11 21:29:24 +0900

    🎨背景イメージをセット

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,20 @@
 import React, { ReactNode } from 'react';
 import { Footer } from './footer';
 import { Header } from './header';
+import { Background } from 'components/Background';
 type Props = {
   children: ReactNode;
 };
 export const Layout: React.FC<Props> = (props) => {
   return (
     <div>
-      <Header />
-      <div>{props.children}</div>
-      <Footer />
+      <Background />
+      <div>
+        <Header />
+
+        <div>{props.children}</div>
+        <Footer />
+      </div>
     </div>
   );
 };
