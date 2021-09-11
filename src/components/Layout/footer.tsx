2a42abf21dc582233c9c01bdabd1b4d9ea7e28a7
kitutune <p.kitutune@gmail.com>
2021-09-11 22:08:05 +0900

    💄 footerを下部に固定

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 export const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className='fixed bottom-0 left-0 w-full h-5 bg-white opacity-20 text-center'>
       <a
         href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
         target='_blank'
