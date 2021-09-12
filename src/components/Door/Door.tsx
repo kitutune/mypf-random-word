a6a3539a2ca46f77202c97b6a3a44943a2176d65
kitutune <p.kitutune@gmail.com>
2021-09-12 18:41:35 +0900

    🎨一旦Doorcomponent完成

--- src/components/Door/Door.tsx
+++ src/components/Door/Door.tsx
@@ -2,15 +2,12 @@ import cc from 'classcat';
 import React, { useState } from 'react';
 import classes from 'components/Door/Door.module.css';
 
-interface Props {
-  checked?: boolean;
-}
 export const Door: React.FC = (props) => {
   const [isShow, setIsShow] = useState(false);
-  const handleDoor = (e, setter) => {
-    console.log('e.target', e.target);
-    console.log('e.currentTarget', e.currentTarget);
-    console.log(setter);
+  const handleDoor = (e: React.MouseEvent) => {
+    // console.log('e.target', e.target);
+    // console.log('e.currentTarget', e.currentTarget);
+    // console.log(setter);
 
     console.log('クリックした');
     setIsShow((pre) => !pre);
@@ -18,31 +15,17 @@ export const Door: React.FC = (props) => {
   console.log(isShow);
 
   return (
-    <div
-      // className={`menuWrapper ${isShow ? 'menuWrapper__active' : ''}`}
-      onClick={(e) => {
-        handleDoor(e, setIsShow);
-        // handleDoor(e);
-      }}
-      // onClick={handleDoor}
-    >
-      <h1>CSSで画像を真っ二つに切り開く</h1>
-      <input
-        type='checkbox'
-        checked={isShow}
-        className={`${classes.check} ${classes.slide}`}
-        id='checked-slide'
-      />
-
-      <label className={`${classes.switch} ${classes.slide}`} htmlFor='checked-slide'></label>
+    <div onClick={handleDoor}>
+      <h1>上</h1>
+      <input type='checkbox' checked={isShow} className={`${classes.check} ${classes.slide}`} />
 
       <div className={classes.gatefold}>
         <div className={classes.header}>
-          <h1>CSSで画像を真っ二つに切り開く</h1>
+          <h1>真ん中</h1>
           {props.children}
         </div>
       </div>
-      <h1>CSSで画像を真っ二つに切り開く</h1>
+      <h1>した</h1>
     </div>
   );
 };
