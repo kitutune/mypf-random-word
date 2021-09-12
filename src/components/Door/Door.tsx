a40fa68659045368978b96961cbc12f57d9b4194
kitutune <p.kitutune@gmail.com>
2021-09-12 18:13:09 +0900

    🚧 DoorのギミックをonClickに割り当てる作業中

--- src/components/Door/Door.tsx
+++ src/components/Door/Door.tsx
@@ -1,11 +1,38 @@
 import cc from 'classcat';
 import React, { useState } from 'react';
 import classes from 'components/Door/Door.module.css';
+
+interface Props {
+  checked?: boolean;
+}
 export const Door: React.FC = (props) => {
+  const [isShow, setIsShow] = useState(false);
+  const handleDoor = (e, setter) => {
+    console.log('e.target', e.target);
+    console.log('e.currentTarget', e.currentTarget);
+    console.log(setter);
+
+    console.log('クリックした');
+    setIsShow((pre) => !pre);
+  };
+  console.log(isShow);
+
   return (
-    <div>
+    <div
+      // className={`menuWrapper ${isShow ? 'menuWrapper__active' : ''}`}
+      onClick={(e) => {
+        handleDoor(e, setIsShow);
+        // handleDoor(e);
+      }}
+      // onClick={handleDoor}
+    >
       <h1>CSSで画像を真っ二つに切り開く</h1>
-      <input type='checkbox' className={`${classes.check} ${classes.slide}`} id='checked-slide' />
+      <input
+        type='checkbox'
+        checked={isShow}
+        className={`${classes.check} ${classes.slide}`}
+        id='checked-slide'
+      />
 
       <label className={`${classes.switch} ${classes.slide}`} htmlFor='checked-slide'></label>
 
