2a8e76c90861f90f087ad0026305067b9963b266
kitutune <p.kitutune@gmail.com>
2021-09-12 16:59:35 +0900

    🎨_appにDoorを設置（最初にDoorが表示されるようにする）

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import { Door } from 'components/Door/Door';
 import { Layout } from 'components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='m-0 p-0 w-full h-full'>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Door>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Door>
     </div>
   );
 }
