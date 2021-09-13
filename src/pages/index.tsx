fe807cb5ef24762f8fec618e50beaa24bfb07c5d
kitutune <p.kitutune@gmail.com>
2021-09-13 22:15:59 +0900

    🔥 余分なコードとファイルを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 import { Login } from 'components/Login';
-import { Test } from 'components/Test';
-
 const Home: NextPage = () => {
   return (
     <main>
       amu
-      <Test />
       <Login />
       last
     </main>
