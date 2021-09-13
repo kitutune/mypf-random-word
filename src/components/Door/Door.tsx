b2d3c3df501ebc37be80e1b2e2e2598158bbe3a0
kitutune <p.kitutune@gmail.com>
2021-09-13 22:08:49 +0900

    🎨ユーザーがログインしていたらドアが開くように変更

--- src/components/Door/Door.tsx
+++ src/components/Door/Door.tsx
@@ -1,35 +1,42 @@
-import cc from 'classcat';
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classes from 'components/Door/Door.module.css';
+import { useLogin } from 'components/Hooks/useLogin';
 
 export const Door: React.FC = (props) => {
-  const [isShow, setIsShow] = useState(false);
-  const handleDoor = (e: React.MouseEvent) => {
-    // console.log('e.target', e.target);
-    // console.log('e.currentTarget', e.currentTarget);
-    // console.log(setter);
-
-    console.log('クリックした');
-    setIsShow((pre) => !pre);
-  };
-  console.log(isShow);
-
+  const { session, signInWithGithub, signOut } = useLogin();
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const handleDoor = useCallback(
+    (e: React.MouseEvent) => {
+      if (session) {
+        console.log('a');
+        return;
+      }
+      signInWithGithub();
+      console.log('クリックした');
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isShow],
+  );
+  useEffect(() => {
+    if (session) {
+      setIsShow(true);
+    }
+    return () => {};
+  }, [session]);
   return (
-    <div onClick={handleDoor}>
-      <h1>上</h1>
-      <input
-        type='checkbox'
-        defaultChecked={isShow}
-        className={`${classes.check} ${classes.slide}`}
-      />
-
-      <div className={classes.gatefold}>
-        <div className={classes.header}>
-          <h1>真ん中</h1>
-          {props.children}
+    <>
+      <div onClick={handleDoor}>
+        <input
+          type='checkbox'
+          defaultChecked={isShow}
+          className={`${classes.check} ${classes.slide}`}
+        />
+        <div className={classes.gatefold}>
+          <div className={classes.header}>
+            <div className='w-full h-full'> {props.children}</div>
+          </div>
         </div>
       </div>
-      <h1>した</h1>
-    </div>
+    </>
   );
 };
