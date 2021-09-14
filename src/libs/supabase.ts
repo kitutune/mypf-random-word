07b228d51bd3ef95e8f8dee97621488d232ecfd9
kitutune <p.kitutune@gmail.com>
2021-09-14 21:26:06 +0900

    ♻️ nullの可能性を排除した

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+}
+if (!supabaseAnonKey) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
