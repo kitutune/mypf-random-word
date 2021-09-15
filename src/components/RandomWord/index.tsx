e8623cbb6ffcf1a867f43a77632d27f4984e6ed6
kitutune <p.kitutune@gmail.com>
2021-09-15 17:36:31 +0900

    🐛 useStateのデフォルト値を設定指定なっかので設定した

--- src/components/RandomWord/index.tsx
+++ src/components/RandomWord/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { supabase } from 'libs/supabase';
-
-export const RandomWord = () => {
-  const [messageNo, setMessageNo] = useState();
+export const RandomWord: React.FC = () => {
+  const [messageNo, setMessageNo] = useState(0);
   const [words, setWords] = useState([
     {
       word: '',
@@ -10,33 +9,33 @@ export const RandomWord = () => {
       url: '',
     },
   ]);
-  const messages = ['あああ', 'いいい', 'ううう'];
-  const handleChange = async () => {
-    // const a = Math.floor(Math.random() * messages.length);
-    // setMessageNo(a);
-
+  const getWords = async () => {
     const { data: wordbox, error } = await supabase.from('wordbox').select('*');
     if (wordbox) {
       setWords(wordbox);
     }
-
+  };
+  const handleChange = useCallback(() => {
     const a = Math.floor(Math.random() * words.length);
     setMessageNo(a);
-  };
+  }, []);
   console.log(words);
-  console.log(words[0].id);
-  // console.log(words.id);
-  // console.log(words.word);
+
+  useEffect(() => {
+    getWords();
+    return () => {};
+  }, []);
 
   return (
     <div className='text-6xl'>
       <h1> ランダムワード</h1>
-      {/* <h1>→{messages[messageNo]}←</h1>
-      <h1>→{messageNo}←</h1>
-      <h1>→{[messageNo]}←</h1> */}
+      <div className='text-6xl'>
+        <a href={words[messageNo].url}> {words[messageNo].word}</a>
+      </div>
       <h1>→{words[messageNo].word}←</h1>
       <h1>→{words[messageNo].url}←</h1>
       <h1>→{words[messageNo].id}←</h1>
+
       <button onClick={handleChange}>push</button>
     </div>
   );
