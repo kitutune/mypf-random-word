08a5081adbeea5b276ebbde93570ae6ebf8a8f80
kitutune <p.kitutune@gmail.com>
2021-09-15 19:16:55 +0900

    🎨 Homeへのリンクを追加

--- src/components/Layout/Header/HouseIcon/index.tsx
+++ src/components/Layout/Header/HouseIcon/index.tsx
@@ -1,5 +1,5 @@
+import Link from 'next/link';
 import React from 'react';
-
 export const HouseIcon = () => {
   return (
     <svg
@@ -8,9 +8,13 @@ export const HouseIcon = () => {
       data-name='Capa 1'
       viewBox='0 0 16.16 12.57'
     >
-      <defs></defs>
-      <path d='M14.02 4.77v7.8H9.33V8.8h-2.5v3.77H2.14v-7.8h11.88z'></path>
-      <path d='M16.16 5.82H0L8.08 0l8.08 5.82z'></path>
+      <Link href='/'>
+        <a>
+          <defs></defs>
+          <path d='M14.02 4.77v7.8H9.33V8.8h-2.5v3.77H2.14v-7.8h11.88z'></path>
+          <path d='M16.16 5.82H0L8.08 0l8.08 5.82z'></path>{' '}
+        </a>
+      </Link>
     </svg>
   );
 };
