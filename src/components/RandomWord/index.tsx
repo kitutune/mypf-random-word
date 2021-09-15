63eca4b4dfa36d2a6062a0440874f9714fd7b70d
kitutune <p.kitutune@gmail.com>
2021-09-15 14:53:23 +0900

    ✨ ボタンを押すとランダムに配列の中の値が表示される機能を作成

--- src/components/RandomWord/index.tsx
+++ src/components/RandomWord/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const RandomWord = () => {
+  const [messageNo, setMessageNo] = useState();
+  const messages = ['あああ', 'いいい', 'ううう'];
+  const handleChange = () => {
+    const a = Math.floor(Math.random() * messages.length);
+    setMessageNo(a);
+  };
+
   return (
     <div className='text-6xl'>
       <h1> ランダムワード</h1>
+      <h1>→{messages[messageNo]}←</h1>
+      <h1>→{messageNo}←</h1>
+      <h1>→{[messageNo]}←</h1>
+      <button onClick={handleChange}>push</button>
     </div>
   );
 };
