fa4f6daed87fe6c6b995e6bfcd2c85a6e5a58815
kitutune <p.kitutune@gmail.com>
2021-09-15 13:19:08 +0900

    🎨 入力欄の初期値を空文字にしてplaceholderが反映されるようにした

--- src/components/Submission/index.tsx
+++ src/components/Submission/index.tsx
@@ -38,7 +38,7 @@ export const Submission: React.FC = () => {
       <div className='min-h-screen  flex items-center'>
         <div className='container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300'>
           <div className='py-12 p-10 bg-white rounded-xl'>
-            {Object.entries(form).map(([value], i) => (
+            {Object.entries(form).map(([value, defaultValue], i) => (
               <div className='mb-6' key={i}>
                 <label
                   className='mr-4 uppercase text-gray-700 font-bold inline-block mb-2'
@@ -50,6 +50,7 @@ export const Submission: React.FC = () => {
                   type='text'
                   className='border text-xs text-black bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded'
                   placeholder={holder[i]}
+                  value={defaultValue}
                   onChange={handleChange(value)}
                 />
               </div>
