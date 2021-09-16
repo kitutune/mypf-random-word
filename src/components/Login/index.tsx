523c2ba0a9707c74d95365520dd57b44b1dbf5a7
kitutune <p.kitutune@gmail.com>
2021-09-16 19:07:36 +0900

    🔥 必要のないコードを削除

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,13 +1,5 @@
 import { useLogin } from 'components/Hooks/useLogin';
 export const Login = () => {
-  const { session, signInWithGithub, signOut } = useLogin();
-  return (
-    <>
-      {session ? (
-        <button onClick={() => signOut()}>サインアウト</button>
-      ) : (
-        <button onClick={() => signInWithGithub()}>GitHubでログイン</button>
-      )}
-    </>
-  );
+  const { signOut } = useLogin();
+  return <button onClick={() => signOut()}>サインアウト</button>;
 };
