e7e94285d1dbd2c9720d0411d527813effa2afa7
kitutune <p.kitutune@gmail.com>
2021-09-17 03:25:39 +0900

    ✨ componentを変更すると背景も変わるようにした

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,8 +1,22 @@
 import Image from 'next/image';
 import classes from 'components/Background/Background.module.css';
+import { useTab } from 'components/Hooks/useTab';
 
-export const Background = () => (
-  <div className={classes.bgWrap}>
-    <Image alt='Mountains' src='/night.jpeg' layout='fill' objectFit='cover' quality={100} />
-  </div>
-);
+export const Background = () => {
+  const { tab } = useTab();
+  console.log(tab);
+
+  return (
+    <div className={classes.bgWrap}>
+      {tab.tab3 ? (
+        <Image alt='morning' src='/morning.jpeg' layout='fill' objectFit='cover' quality={100} />
+      ) : null}
+      {tab.tab2 ? (
+        <Image alt='noon' src='/afternoon.jpeg' layout='fill' objectFit='cover' quality={100} />
+      ) : null}
+      {tab.tab1 ? (
+        <Image alt='night' src='/night.jpeg' layout='fill' objectFit='cover' quality={100} />
+      ) : null}
+    </div>
+  );
+};
