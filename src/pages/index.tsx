75bc8b30691a255f8980ca1b561d4672f9f3aa10
kitutune <p.kitutune@gmail.com>
2021-09-16 16:05:34 +0900

    🎨 userIdをpageで管理した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next';
+import { useLogin } from 'components/Hooks/useLogin';
 import { useTab } from 'components/Hooks/useTab';
 import { MyWordList } from 'components/MyWordList';
 import { RandomWord } from 'components/RandomWord';
 import { Submission } from 'components/Submission';
 
+export type Props = {
+  userId: string;
+};
+
 const Home: NextPage = () => {
   const { tab } = useTab();
-  console.log(tab);
-  console.log(tab.tab1);
+  const { userId } = useLogin();
 
   return (
     <main>
       {tab.tab1 ? <RandomWord /> : null}
-      {tab.tab2 ? <MyWordList /> : null}
-      {tab.tab3 ? <Submission /> : null}
+          {tab.tab2 ? <MyWordList userId={userId} /> : null}
+          {tab.tab3 ? <Submission userId={userId} /> : null}
     </main>
   );
 };
