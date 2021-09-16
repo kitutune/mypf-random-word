888dd3f7de37c65615b4e0bbec7bcb307f8ea44a
kitutune <p.kitutune@gmail.com>
2021-09-16 17:36:29 +0900

    🎨 自分が登録したリンクに飛べるようにした

--- src/components/MyWordList/index.tsx
+++ src/components/MyWordList/index.tsx
@@ -51,9 +51,14 @@ export const MyWordList = (props: Props) => {
       {/* {Object.entries(words).map(([value, defaultValue], i) => ( */}
       {myWords.map((myData, i) => (
         <div className='flex flex-wrap m-auto' key={i}>
-          <div className='w-3/5 rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white'>
+          <a
+            href={myData.url}
+            target='_blank'
+            rel='noreferrer noopener'
+            className='w-3/5 rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white'
+          >
             {myData.word}
-          </div>
+          </a>
           <button
             className='w-1/5 rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800'
             onClick={() => deleteButton(myData.id)}
