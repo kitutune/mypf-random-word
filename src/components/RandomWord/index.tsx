8f53c3d7ab6d1537a3763ccc02084f592b29f410
kitutune <p.kitutune@gmail.com>
2021-09-16 15:55:39 +0900

    🐛 supabaseが[0]を引く時だけ一部データが欠けるので[0]を回避処置

--- src/components/RandomWord/index.tsx
+++ src/components/RandomWord/index.tsx
@@ -4,10 +4,13 @@ export const RandomWord: React.FC = () => {
   const [messageNo, setMessageNo] = useState(0);
   const { getWords, words } = useWordLists();
   const handleChange = useCallback(() => {
-    const a = Math.floor(Math.random() * words.length);
-    setMessageNo(a);
+    const number = Math.floor(Math.random() * words.length);
+    // supabaseのデータを参照する際、0の値のwordが空文字になるので回避処理
+    if (number === 0) {
+      return setMessageNo(1);
+    }
+    setMessageNo(number);
   }, [words]);
-  console.log(words);
 
   useEffect(() => {
     getWords();
