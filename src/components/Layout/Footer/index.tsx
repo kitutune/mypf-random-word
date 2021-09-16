f8ad5ff71b1f87bcc1930617e8544a5182012844
kitutune <p.kitutune@gmail.com>
2021-09-17 01:55:58 +0900

    🎨 GithubIcon設定　リンク先はgithub

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,17 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 export const Footer: React.FC = () => {
   return (
-    <footer className='fixed bottom-0 left-0 w-full h-5 bg-white opacity-20 text-center'>
+    <footer className='fixed bottom-0 left-0 w-full h-5 bg-gray-500 opacity-80 text-center'>
       <a
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        href='https://github.com/kitutune/mypf-random-word'
         target='_blank'
         rel='noopener noreferrer'
+        className='absolute bottom-0 left-auto right-auto'
       >
-        Powered by
-        <span>
-          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </span>
+        <AiFillGithub />
       </a>
     </footer>
   );
