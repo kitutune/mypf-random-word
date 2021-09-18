d1dc04911c42ccc5fbd6d511591ae4ae2b9e9c74
kitutune <p.kitutune@gmail.com>
2021-09-19 02:46:11 +0900

    🎨 コードの修正

--- src/components/Hooks/useTab.tsx
+++ src/components/Hooks/useTab.tsx
@@ -1,15 +1,12 @@
 import useSWR from 'swr';
 
-const useSharedState = (key: any, fallbackData: any) => {
+const useSharedState = (key: string, fallbackData: any) => {
   const { data, mutate } = useSWR(key, { fallbackData });
   return [data, mutate];
 };
 
 export const useTab = () => {
-  const [tab, setTab] = useSharedState(
-    { tab1: 'tab1', tab2: 'tab2', tab3: 'tab3' },
-    { tab1: true, tab2: false, tab3: false },
-  );
+  const [tab, setTab] = useSharedState('tabsState', { tab1: true, tab2: false, tab3: false });
 
   const RandomWordOn = () => {
     setTab({ tab1: true, tab2: false, tab3: false });
