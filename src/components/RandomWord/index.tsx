df7108f43cb8c30d0347aef6f429ee19c9eb3c49
kitutune <p.kitutune@gmail.com>
2021-09-19 02:20:46 +0900

    🎨 テキストの文字を白に設定

--- src/components/RandomWord/index.tsx
+++ src/components/RandomWord/index.tsx
@@ -27,7 +27,7 @@ export const RandomWord = (props: Props) => {
   return (
     <div className='w-10/12 lg:text-6xl sm:text-2xl'>
       <h1> ランダムワード</h1>
-      <div className='lg:text-6xl sm:text-2xl'>
+      <div className='lg:text-6xl sm:text-2xl text-white'>
         <a href={props.words[messageNo].url} target='_blank' rel='noreferrer noopener'>
           {props.words[messageNo].word}
         </a>
